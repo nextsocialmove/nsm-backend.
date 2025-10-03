129da02b564755a9e1248c03a0a1ab5353b88579
nextsocialmove <nextsocialmove@gmail.com>
2025-10-03 16:25:42 -0500

    Delete api/health.ts

--- api/health.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader("Content-Type", "application/json");
-  res.status(200).send(JSON.stringify({ ok: true }));
-}
