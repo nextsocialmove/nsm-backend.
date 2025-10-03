55fc29ff18cc2f6dd6b4cdf70e86c4e8e062fe7d
nextsocialmove <nextsocialmove@gmail.com>
2025-10-03 16:22:29 -0500

    Create health.ts

---
+++ api/health.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default async function handler(_req: VercelRequest, res: VercelResponse) {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).send(JSON.stringify({ ok: true }));
+}
